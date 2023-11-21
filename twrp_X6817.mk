bd253743190864787fbf9cc15ecda431d0314660
Massatrio16 <146039181+Massatrio16@users.noreply.github.com>
2023-11-21 21:20:24 +0700

    Update twrp_X6817.mk

--- twrp_X6817.mk
+++ twrp_X6817.mk
@@ -5,12 +5,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Inherit from lion device
 $(call inherit-product, device/infinix/X6817/device.mk)
 
